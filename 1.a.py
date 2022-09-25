bd8a243325f1e470042b6e6ba1838353d91b9645
sumanthvelagala <70264946+sumanthvelagala@users.noreply.github.com>
2022-09-25 19:45:38 +0530

    Add files via upload

---
+++ 1.a.py
@@ -0,0 +1,16 @@
+
+a=100
+b=10.23
+c=100+3j
+d="Hello Pyhton"
+e=[2,4,6,8]
+f=("hello",1,2,3,"go")
+g={1:"jan",2:"feb",3:"march"}
+print("the type of variable having value",a,"is",type(a))
+print("the type of variable having value",b,"is",type(b))
+print("the type of variable having value",c,"is",type(c))
+print("the type of variable having value",d,"is",type(d))
+print("the type of variable having value",e,"is",type(e))
+print("the type of variable having value",f,"is",type(f))
+print("the type of variable having value",g,"is",type(g))
+
